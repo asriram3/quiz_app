ccf410772b3a8ab6fa5359e7fd14ddd559f695c9
Adi <asriram3@illinois.edu>
2020-01-08 23:39:46 +0800

    privatized

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-var qIndex = 0;
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  void answerChosen() {
-    qIndex = 1 - qIndex;
-    setState(() {});
-    print(qIndex);
+class _MyAppState extends State<MyApp> {
+  var _qIndex = 0;
+
+  void _answerChosen() {
+    // qIndex++;
+    setState(() {
+      if (_qIndex < 1) _qIndex++;
+    });
+    print(_qIndex);
   }
 
   @override
@@ -31,10 +34,10 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(questions[qIndex]),
+            Text(questions[_qIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerChosen,
+              onPressed: _answerChosen,
             ),
             RaisedButton(
               child: Text('Answer 2'),
